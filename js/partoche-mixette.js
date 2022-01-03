6dc1f3fd8ad49af2296bb12dad71087c084d3f95
antoineterny <63339704+antoineterny@users.noreply.github.com>
2022-01-04 00:38:37 +0100

    mixette apparence fonctionnelle

--- js/partoche-mixette.js
+++ js/partoche-mixette.js
@@ -32,7 +32,7 @@ function initAudio(index) {
   for (let i = 0; i < tracks.length; i++) {
     tracks[i].on("load", function () {
       // if (i != 0) tracks[i].mute(true)
-      console.log(tracks[i]._src, "is loaded");
+      // console.log(tracks[i]._src, "is loaded");
       loadedTracks += 1
       onceAudioLoaded()
     })
@@ -49,6 +49,7 @@ function initAudio(index) {
 
     // createVoiceButtons();
     createTitreMarkers()
+    createMixetteControls(voices)
 
     // stopped = false;
     tracks[0].on("end", () => {
@@ -57,4 +58,42 @@ function initAudio(index) {
       // cancelAnimationFrame(animID);
     })
   }
+}
+
+function createMixetteControls(voices) {
+  voices.forEach(voice => {
+    console.log(`voice`, voice, typeof voice)
+    document.querySelector("#pages").innerHTML += `
+      <div class="tranche" id="${voice}">
+      <div class="slider-wrapper">
+        <div class="slider-scale">
+          <input
+            class="volume-slider"
+            type="range"
+            id="volume-${voice}"
+            min="0"
+            max="100"
+            value="100"
+            oninput="updateVolume('${voice}')"
+          />
+        </div>
+      </div>
+      <label id="label-${voice}">100%</label>
+      <div class="solo-mute-btns">
+        <button class="solo-btn" id="solo-${voice}" onclick="soloVoice('${voice}')">S</button>
+        <button class="mute-btn" id="mute-${voice}" onclick="muteVoice('${voice}')">M</button>
+      </div>
+      <p>${voice}</p>
+    </div>`
+  })
+}
+
+function soloVoice(voice) {
+  document.getElementById(`solo-${voice}`).classList.toggle('soloed')
+}
+function muteVoice(voice) {
+  document.getElementById(`mute-${voice}`).classList.toggle('muted')
+}
+function updateVolume(voice) {
+  document.getElementById(`label-${voice}`).innerText = document.getElementById(`volume-${voice}`).value + "%"
 }
